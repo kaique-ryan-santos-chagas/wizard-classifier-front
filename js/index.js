e9639cb4c11f771e4aab765b52dd80a3ee7649eb
HITSS <Usuario@SPONB3702.globalhitss.com.br>
2024-01-30 16:34:29 -0300

    Trabalhando no código do jogo.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,29 @@
+var page = document.documentElement;
 var menu_container = document.getElementById('menu');
 var library_audio = document.getElementById('royal_library');
+var paper_audio = document.getElementById('paper');
 var start_button = document.getElementById('start');
+var witch = document.getElementById('witch');
 
-start_button.onclick = () => {
-    library_audio.play();
-    menu_container.style.display = 'none';
+start_button.onclick = () => {    
+    paper_audio.play();
+    menu_container.style.animationName = 'animation-top';
+    menu_container.style.animationDuration = '1s';
+    
+    setTimeout(() => {
+        
+        library_audio.play();
+        page.requestFullscreen();
+        menu_container.style.display = 'none';
+        witch.style.display = 'flex';
 
+        page.onfullscreenchange = () => {
+            witch.style.marginTop = '20%';
+        }
+
+    }, 800);
+    
 }
+
+
 
