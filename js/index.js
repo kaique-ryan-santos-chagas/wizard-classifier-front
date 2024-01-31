5acdfbba389d519b5f6cff863afd398b10022fdf
Kaique Ryan Santos Chagas <kaique.chagas@globalhitss.com.br>
2024-01-31 18:06:09 -0300

    Trabalhando no controle dos eventos do jogo

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ var library_audio = document.getElementById('royal_library');
 var paper_audio = document.getElementById('paper');
 var start_button = document.getElementById('start');
 var witch = document.getElementById('witch');
+var witch_dialog = document.getElementById('witch_dialog');
+var dialog_ballon = document.getElementById('dialog_ballon');
 
 start_button.onclick = () => {    
     
@@ -19,13 +21,25 @@ start_button.onclick = () => {
 
     }, 800);
     
+    setTimeout(() => {
+        
+        witch_dialog.style.display = 'flex'
+        witch_dialog.style.animationName = 'animation-bottom';
+        witch_dialog.style.animationDuration = '1.5s';
+        setInterval(changeDialogMessage, 10000);
+
+    }, 2000);
 }
 
 document.documentElement.onfullscreenchange = () => {
+    
     if(document.fullscreenElement !== null)
         witch.style.marginTop = '20%';
-    else
-        witch.style.marginTop = '10%';
+    else {
+        witch.style.marginTop = '15%';
+        dialog_ballon.style.width = '400px';
+        witch_dialog.style.marginLeft = '60%';
+    }
 }
 
   
